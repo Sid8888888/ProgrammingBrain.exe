92a38571b46e90718d95f020344ce0d37569d6f0
SuganSid <sugansugumar2000@gmail.com>
2024-12-03 10:10:48 +0530

    .

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -10,17 +10,16 @@ const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
-// Enable CORS for specific origin and allow necessary headers and methods
 app.use(cors({
   origin: ['http://localhost:3000', 'http://localhost:3001'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// Handle preflight requests for all routes
+
 app.options('*', cors());
 
-// Service Management
+
 
 app.use("/auth", require("./Order/admin-auth/userRoutes"));
 app.use("/", require("./Order/Customer/CustomerRoutes"));
